2a1a5fc76fd451d7c5cd6848439545512f69cc54
Zach Nusbaum <zach@zdnmail.com>
2018-11-20 07:17:28 -0800

    Try adding cloud function with external dependency

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,12 @@
 import * as functions from 'firebase-functions';
-
+import { ulid } from 'ulid';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
+
+export const generateULID = functions.https.onRequest((request, response) => {
+  response.send(ulid());
+})
