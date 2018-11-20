ab321bc2dfaec15b955d293befa58ffa6155c94a
Zach Nusbaum <zach@zdnmail.com>
2018-11-20 07:53:38 -0800

    Add google login component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { StoreModule } from '@ngrx/store';
 import * as fromAuth from './store/reducers/auth.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './store/effects/auth.effects';
+import { GoogleLoginComponent } from './components/google-login/google-login.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [GoogleLoginComponent],
   imports: [
     CommonModule,
     StoreModule.forFeature('auth', fromAuth.reducer),
--- src/app/auth/store/reducers/auth.reducer.ts
+++ src/app/auth/store/reducers/auth.reducer.ts
@@ -2,11 +2,11 @@ import { Action } from '@ngrx/store';
 import { AuthActions, AuthActionTypes } from '../actions/auth.actions';
 
 export interface State {
-
+  loggedIn: boolean;
 }
 
 export const initialState: State = {
-
+  loggedIn: false
 };
 
 export function reducer(state = initialState, action: AuthActions): State {
